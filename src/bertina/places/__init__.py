84ea9e3ea567f41f9059e1acd2ab3772765a91c6
Amir_Kimiaei <a.m.kimiaei84@gmail.com>
2026-05-20 13:05:31 +0000

    add places __init__ exports

---
+++ src/bertina/places/__init__.py
@@ -0,0 +1,12 @@
+from .client import AsyncBertinaPlaces, BertinaPlaces
+from .models import City, PlaceCard, PlaceDetail, PlacesCategory, Province
+
+__all__ = [
+    "BertinaPlaces",
+    "AsyncBertinaPlaces",
+    "Province",
+    "City",
+    "PlacesCategory",
+    "PlaceCard",
+    "PlaceDetail",
+]
