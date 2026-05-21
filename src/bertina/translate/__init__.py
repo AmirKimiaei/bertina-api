babf19de67804452d17a2fbab68ebfad1d7cee7b
Amir_Kimiaei <a.m.kimiaei84@gmail.com>
2026-05-21 03:51:44 +0000

    update translate __init__ exports

---
+++ src/bertina/translate/__init__.py
@@ -0,0 +1,5 @@
+from .client import AsyncBertinaTranslate, BertinaTranslate
+from .languages import Language
+from .models import TranslationResult
+
+__all__ = ["BertinaTranslate", "AsyncBertinaTranslate", "Language", "TranslationResult"]
